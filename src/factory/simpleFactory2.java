91e74ce16bf97ce4dc849199e77e365568a60876
ahucsxl <ahucsxl@126.com>
2016-04-11 16:06:11 +0800

    modifiel simpleFactory2

--- src/factory/simpleFactory2.java
+++ src/factory/simpleFactory2.java
@@ -1,5 +1,41 @@
 package factory;
 
 public class simpleFactory2 {
-//reset
+	public static void main(String[] args) {
+		Car c = Factory.getInstance("Bmw");
+		c.drive();
+
+	}
+}
+interface Car {
+	public void drive();
+}
+
+class Benz implements Car {
+
+	public void drive() {
+		System.out.println("Benz");
+	}
+}
+
+class Bmw implements Car {
+	@Override
+	public void drive() {
+		System.out.println("drive Bmw");
+
+	}
+
+}
+
+class Factory {
+	public static Car getInstance(String s) {
+		switch (s) {
+		case "Benze":
+			return new Benz();
+		case "Bmw":
+			return new Bmw();
+		default:
+			return null;
+		}
+	}
 }
